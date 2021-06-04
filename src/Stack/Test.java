09cc05150981baf5dcab06b9ad765654015de44f
777AMR <meyram.ramazanov@mail.ru>
2021-06-04 17:46:17 +0600

    //Iterator

---
+++ src/Stack/Test.java
@@ -0,0 +1,19 @@
+package Stack;
+
+import java.util.Stack;
+
+public class Test {
+    public static void main(String[] args) {
+        Stack<Integer> stack = new Stack<>();
+
+        stack.push(5);
+        stack.push(3);
+        stack.push(1);
+//      получает последний элемент
+//        System.out.println(stack.pop());
+
+        while (!stack.isEmpty()) {
+            System.out.println(stack.pop());
+        }
+    }
+}
