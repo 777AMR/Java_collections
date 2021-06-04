5ae8b246c8548ad1df1041a8664353f4e62a43c3
777AMR <meyram.ramazanov@mail.ru>
2021-06-04 11:19:38 +0600

    //Maps and Set

---
+++ src/Map/AllMap.java
@@ -0,0 +1,37 @@
+package Map;
+
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.TreeMap;
+
+public class AllMap {
+    public static void main(String[] args) {
+        //внутри не гарантируется никакого порядка
+        Map<Integer, String> hashMap = new HashMap<>();
+
+        //в каком порядке пары (ключ *) значение были добавлены, в таком порядке они и вернутся
+        Map<Integer, String> linkedHashMap = new LinkedHashMap<>();
+
+        //в пары(ключ*значение) сортируется по ключу(естественный порядок)
+        Map<Integer, String> treeMap = new TreeMap<>();//в пары(ключ*значение) сортируется по ключу(естественный порядок)
+
+//        testMap(hashMap);
+//        testMap(linkedHashMap);
+        testMap(treeMap);
+    }
+
+    public static void testMap(Map<Integer, String> map) {
+        map.put(39, "Bob");
+        map.put(12, "Mike");
+        map.put(78, "Tom");
+        map.put(0, "Tim");
+        map.put(1500, "Lewis");
+        map.put(7, "George");
+
+        for (Map.Entry<Integer, String> entry : map.entrySet()) {
+            System.out.println(entry.getKey() + " : " + entry.getValue());
+        }
+    }
+
+}
---
+++ src/Set/AllSet.java
@@ -0,0 +1,92 @@
+package Set;
+
+import java.util.HashSet;
+import java.util.LinkedHashSet;
+import java.util.Set;
+import java.util.TreeSet;
+
+public class AllSet {
+    public static void main(String[] args) {
+        Set<Integer> hashSet = new HashSet<>();
+
+        hashSet.add(0);
+        hashSet.add(1);
+        hashSet.add(2);
+        hashSet.add(3);
+        hashSet.add(4);
+        hashSet.add(5);
+
+        Set<Integer> hashSet2 = new HashSet<>();
+        hashSet2.add(2);
+        hashSet2.add(3);
+        hashSet2.add(4);
+        hashSet2.add(5);
+        hashSet2.add(6);
+        hashSet2.add(7);
+
+        // union - объединение множеств
+        Set<Integer> union = new HashSet<>(hashSet);
+        union.addAll(hashSet2);
+        System.out.println(union);
+
+        //intersection - пересечение множеств
+        Set<Integer> intersection = new HashSet<>(hashSet);
+        intersection.retainAll(hashSet2);
+        System.out.println(intersection);
+
+        //difference - разность множеств
+        Set<Integer> difference = new HashSet<>(hashSet);
+        difference.removeAll(hashSet2);
+        System.out.println(difference);
+
+
+//        Set<String> hashSet = new HashSet<>();
+//        LinkedHashSet<String> linkedHashSet = new LinkedHashSet<>();
+//        TreeSet<String> treeSet = new TreeSet<>();
+//        hashSet.add("Mike");
+//        hashSet.add("Katy");
+//        hashSet.add("Tom");
+//        hashSet.add("George");
+//        hashSet.add("Donald");
+//        hashSet.add("Zelda");
+
+//        System.out.println(hashSet);
+//
+//        System.out.println(hashSet.contains("Tom"));
+//        System.out.println(hashSet.contains("Tim"));
+//
+//        System.out.println(hashSet.isEmpty());
+
+//        1.hashSet
+//        hashSet.add("Mike");
+//        hashSet.add("Katy");
+//        hashSet.add("Tom");
+//        hashSet.add("George");
+//        hashSet.add("Donald");
+//        for (String name : hashSet) {
+//            System.out.println(name);
+//        }
+
+//        2.linkedHashSet
+//        linkedHashSet.add("Mike");
+//        linkedHashSet.add("Katy");
+//        linkedHashSet.add("Tom");
+//        linkedHashSet.add("George");
+//        linkedHashSet.add("Donald");
+//
+//        for (String name : linkedHashSet) {
+//            System.out.println(name);
+//        }
+
+//        3.treeSet
+//        treeSet.add("Mike");
+//        treeSet.add("Katy");
+//        treeSet.add("Tom");
+//        treeSet.add("George");
+//        treeSet.add("Donald");
+//
+//        for (String name : treeSet) {
+//            System.out.println(name);
+//        }
+    }
+}
