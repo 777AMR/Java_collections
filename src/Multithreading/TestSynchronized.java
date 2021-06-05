86c19266e8ef34ab3f79ead4c5af431f4a9fbf8a
777AMR <meyram.ramazanov@mail.ru>
2021-06-05 20:01:35 +0600

    //Multithreading - Synchronized

---
+++ src/Multithreading/TestSynchronized.java
@@ -0,0 +1,43 @@
+package Multithreading;
+
+public class TestSynchronized {
+    private int counter;
+
+    public static void main(String[] args) throws InterruptedException {
+        TestSynchronized testSynchronized = new TestSynchronized();
+        testSynchronized.doWork();
+    }
+
+    //1: 100 -> 101 -> 101
+    //2: 100 -> 101 -> 101
+
+    public synchronized void increment(){
+        counter++;
+    }
+
+    public void doWork() throws InterruptedException {
+        Thread thread1 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                for (int i = 0; i < 10000; i++)
+                    increment();
+            }
+        });
+
+        Thread thread2 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                for (int i = 0; i < 10000; i++)
+                    increment();
+            }
+        });
+
+        thread1.start();
+        thread2.start();
+
+        thread1.join();
+        thread2.join();
+
+        System.out.println(counter);
+    }
+}
